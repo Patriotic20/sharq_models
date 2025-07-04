a5c0acbac5ae4121800e76ff7c2c0914f87d54f6
Diyorbek-Farhodov <tuchun12@gmail.com>
2025-07-04 12:59:49 +0500

    Inital commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sharqmodel!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ sharq_models/__init__.py
@@ -0,0 +1,21 @@
+__all__ = (
+    "StudyDirection",
+    "StudyLanguage",
+    "StudyForm",
+    "User",
+    "PassportData",
+    "StudyInfo",
+    "Application",
+)
+
+
+
+from models import (
+    StudyDirection,
+    StudyLanguage,
+    StudyForm,
+    User,
+    PassportData,
+    StudyInfo,
+    Application,
+)
